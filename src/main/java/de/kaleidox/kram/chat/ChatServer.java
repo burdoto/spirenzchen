eca6a16adaab255ae7f79928bfa41a7e818fff5b
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-01-23 18:30:39 +0100

    fixes

--- src/main/java/de/kaleidox/kram/chat/ChatServer.java
+++ src/main/java/de/kaleidox/kram/chat/ChatServer.java
@@ -3,8 +3,6 @@ package de.kaleidox.kram.chat;
 import org.comroid.api.ContextualProvider;
 import org.comroid.api.os.OS;
 import org.comroid.restless.REST;
-import org.comroid.restless.adapter.java.JavaHttpAdapter;
-import org.comroid.uniform.adapter.json.fastjson.FastJSONLib;
 import org.comroid.webkit.model.PagePropertiesProvider;
 import org.comroid.webkit.server.WebkitServer;
 
@@ -12,12 +10,14 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.ForkJoinPool;
+import java.util.stream.IntStream;
 
 public final class ChatServer implements PagePropertiesProvider, ContextualProvider.Underlying {
     public static final int DEFAULT_PORT_BASE = 42420;
     public static final String URL_BASE = "http://comroid.org:" + DEFAULT_PORT_BASE;
-    public static final ContextualProvider CONTEXT = FastJSONLib.fastJsonLib.plus("static ChatServer", new JavaHttpAdapter());
+    public static final ContextualProvider CONTEXT = ContextualProvider.getRoot();
     public static ChatServer instance;
     private final ContextualProvider context;
     private final WebkitServer webkit;
@@ -51,6 +51,42 @@ public final class ChatServer implements PagePropertiesProvider, ContextualProvi
     }
 
     public static void main(String[] args) throws IOException {
+        Random rng = new Random();
+        IntStream.generate(rng::nextInt)
+                .limit(20)
+                .map(x -> x % 10)
+                .mapToObj(x -> {
+                    switch (x) {
+                        case 0:
+                            return "zero=0";
+                        case 1:
+                            return "one=1";
+                        case 2:
+                            return "two=2";
+                        case 3:
+                            return "three=3";
+                        case 4:
+                            return "four=4";
+                        case 5:
+                            return "five=5";
+                        case 6:
+                            return "six=6";
+                        case 7:
+                            return "seven=7";
+                        case 8:
+                            return "eight=8";
+                        case 9:
+                            return "nine=9";
+                        default:
+                            return "kp=-1";
+                    }
+                })
+                .map(str -> str.split("=")[1])
+                .mapToInt(Integer::parseInt)
+                .filter(x -> x < 5)
+                .forEach(System.out::println);
+
+
         instance = new ChatServer(
                 OS.isWindows
                         ? InetAddress.getLocalHost()
