b0c549b1d89411ce47c24cd18471103f32dd6d99
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-01-23 23:11:53 +0100

    Update CardGame.java

--- src/main/java/de/kaleidox/kram/cards/CardGame.java
+++ src/main/java/de/kaleidox/kram/cards/CardGame.java
@@ -87,70 +87,75 @@ public class CardGame {
                 if (line.isEmpty())
                     continue;
                 var cmds = line.split(" ");
-
-                Player player = getCurrentPlayer();
-                switch (cmds[0]) {
-                    case "exit":
-                        return;
-                    case "pass":
-                        pass();
-                        break;
-                    case "table":
-                    case "top":
-                        Stack<Card> from;
-                        var iter = Arrays.stream(cmds).iterator();
-                        boolean table = iter.next().equals("table");
-                        int idx = 0;
-                        var arg0 = !iter.hasNext() ? null : iter.next();
-                        if ("table".equals(arg0))
-                            table = true;
-                        var arg1 = !iter.hasNext() ? null : iter.next();
-                        if ((table && arg0 != null && arg0.matches("\\d+"))
-                                || (arg1 != null && arg1.matches("\\d+")))
-                            idx = Integer.parseInt(arg1 != null ? arg1 : arg0);
-                        from = (table ? this.table[idx] : players[Math.max(0, idx - 1)]);
-                        System.out.printf("Top card in %s: %s%n",
-                                table ? "Table " + idx : from,
-                                from.empty() ? "nothing" : from.peek());
-                        break;
-                    case "players":
-                        for (Player plr : players)
-                            System.out.println(plr);
-                        break;
-                    case "deck":
-                        for (Card card : deck)
-                            System.out.println(card);
-                        break;
-                    case "draw":
-                        int drawn = player.draw(
-                                from = cmds.length == 3
-                                        ? players[Math.max(0, Integer.parseInt(cmds[2]) - 1)]
-                                        : deck,
-                                cmds.length == 2
-                                        ? Integer.parseInt(cmds[1])
-                                        : 1);
-                        System.out.printf("Drawn %d cards from %s%n", drawn, from.toString());
-                        break;
-                    case "play":
-                        idx = cmds.length >= 2 ? Integer.parseInt(cmds[1]) : 0;
-                        var tgt = cmds.length >= 3 ? Integer.parseInt(cmds[2]) : 0;
-                        player.play(this, idx, this.table[tgt]);
-                        pass();
-                        break;
-                    case "hand":
-                        List<Card> hand = players[cmds.length == 2
-                                ? Math.max(0, Integer.parseInt(cmds[1]) - 1)
-                                : currentPlayer];
-                        hand.sort(Card::compareTo);
-                        for (int i = 0; i < hand.size(); i++)
-                            System.out.printf("%d\t- %s%n", i, hand.get(i));
-                        break;
-                    default:
-                        type.handleLine(this, line, cmds);
-                }
+                handleCmds(cmds);
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private void handleCmds(String... cmds) {
+        var player = getCurrentPlayer();
+        switch (cmds[0]) {
+            case "exit":
+                return;
+            case "pass":
+                pass();
+                break;
+            case "table":
+            case "top":
+                Stack<Card> from;
+                var iter = Arrays.stream(cmds).iterator();
+                boolean table = iter.next().equals("table");
+                int idx = 0;
+                var arg0 = !iter.hasNext() ? null : iter.next();
+                if ("table".equals(arg0))
+                    table = true;
+                var arg1 = !iter.hasNext() ? null : iter.next();
+                if ((table && arg0 != null && arg0.matches("\\d+"))
+                        || (arg1 != null && arg1.matches("\\d+")))
+                    idx = Integer.parseInt(arg1 != null ? arg1 : arg0);
+                from = (table ? this.table[idx] : players[Math.max(0, idx - 1)]);
+                System.out.printf("Top card in %s: %s%n",
+                        table ? "Table " + idx : from,
+                        from.empty() ? "nothing" : from.peek());
+                break;
+            case "players":
+                for (Player plr : players)
+                    System.out.println(plr);
+                break;
+            case "deck":
+                for (Card card : deck)
+                    System.out.println(card);
+                break;
+            case "draw":
+                int drawn = player.draw(
+                        from = cmds.length == 3
+                                ? players[Math.max(0, Integer.parseInt(cmds[2]) - 1)]
+                                : deck,
+                        cmds.length == 2
+                                ? Integer.parseInt(cmds[1])
+                                : 1);
+                System.out.printf("Drawn %d cards from %s%n", drawn, from.toString());
+                if (drawn == 1)
+                    handleCmds("top");
+                break;
+            case "play":
+                idx = cmds.length >= 2 ? Integer.parseInt(cmds[1]) : 0;
+                var tgt = cmds.length >= 3 ? Integer.parseInt(cmds[2]) : 0;
+                player.play(this, idx, this.table[tgt]);
+                pass();
+                break;
+            case "hand":
+                List<Card> hand = players[cmds.length == 2
+                        ? Math.max(0, Integer.parseInt(cmds[1]) - 1)
+                        : currentPlayer];
+                hand.sort(Card::compareTo);
+                for (int i = 0; i < hand.size(); i++)
+                    System.out.printf("%d\t- %s%n", i, hand.get(i));
+                break;
+            default:
+                type.handleLine(this, String.join(" ", cmds), cmds);
         }
     }
 }
